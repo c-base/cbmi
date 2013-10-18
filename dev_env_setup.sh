63ea1be2c8c727cdc9f8b27cca07f2ebe5a9c7be
Brian Wiborg <baccenfutter@c-base.org>
2013-10-19 01:09:56 +0200

    add dev_env_setup.sh

---
+++ dev_env_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+virtualenv cbmi
+cd cbmi
+source bin/activate
+git clone git@dev.c-base.org:cbmi/cbmi.git src
+cd src
+pip install -r requirements.txt
+
+cat <<EOF
+
+Issue the following SQL statements on your devel MySQLd
+    CREATE DATABASE cbmi DEFAULT CHARACTER SET = utf8;
+    GRANT ALL PRIVILEGES ON cbmi.* TO cbmi@localhost IDENTIFIED BY 'cbmi';
+
+Don't forget to run syncdb:
+    python manage.py syncdb
+
+EOF
